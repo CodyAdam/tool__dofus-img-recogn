69321fd96e4afaaef3f498ca46cf2792c6ff1652
CodyAdam <cody.ad4@gmail.com>
2021-04-05 21:09:35 +0200

    Added python OCR script

---
+++ extractor.py
@@ -0,0 +1,48 @@
+import cv2
+import math
+import os
+
+
+LINE_COUNT = 14
+XP_NAME = 0.07475
+YP_NAME = 0.26667
+WP_NAME = 0.36212
+HP_NAME = 0.46667
+
+
+def ocr_get(img):
+    TEMP_FILE = "./ocr/ocr.png"
+    cv2.imwrite(TEMP_FILE, img)
+    config = "-c page_separator=\"\" -c tessedit_char_whitelist=\"\'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz -\" -l eng"
+    output = os.popen(
+        f'tesseract {TEMP_FILE} stdout --dpi 140 {config}').read()
+    return output.replace('\n', '')
+
+
+def img_crop_p(img, W, H, xp, yp, wp, hp):
+    x = math.floor(xp * W)
+    y = math.floor(yp * H)
+    w = math.floor(wp * W)
+    h = math.floor(hp * H)
+    return img_line[y:y+h, x:x+w]
+
+
+img = cv2.imread("./temp/0.png")
+H = img.shape[0]
+W = img.shape[1]
+
+lineW = W
+lineH = H / LINE_COUNT
+
+data = dict()
+
+for i in range(0, LINE_COUNT):
+    img_line = img[math.floor(
+        lineH * i):math.floor(lineH * (i+1)), 0:0+lineW].copy()
+
+    img_name = img_crop_p(img_line, lineW, lineH, XP_NAME,
+                          YP_NAME, WP_NAME, HP_NAME)
+    # cv2.imshow("img", img_name)
+    # cv2.waitKey(0)
+    name = ocr_get(img_name)
+    print(name)
